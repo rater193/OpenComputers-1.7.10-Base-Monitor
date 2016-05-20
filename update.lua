36537c082901d24a2e2f778d9c90284217eef0b1
rater193 <rater193@gmail.com>
2016-05-20 01:49:06 -0400

    updated json api

--- update.lua
+++ update.lua
@@ -9,7 +9,7 @@ shell.execute('mkdir /api')
 shell.execute('wget -fq "https://raw.githubusercontent.com/rater193/OpenComputers-1.7.10-Base-Monitor/master/api/json.lua" "/api/json.lua"')
 
 --api calls
-dofile("/api/json.lua")
+json = require("json")
 
 --now we get the data from the git repository
 local request, responce = pcall(internet.request, "https://api.github.com/repos/rater193/OpenComputers-1.7.10-Base-Monitor/git/refs")
